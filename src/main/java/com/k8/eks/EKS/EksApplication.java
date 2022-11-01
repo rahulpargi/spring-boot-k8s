e91cffc5adcdec675a9f6964c2c2f64588e3d6db
rahulpargi <rahulpargi@gmail.com>
2022-11-01 12:52:50 -0500

    Initial Commit

---
+++ src/main/java/com/k8/eks/EKS/EksApplication.java
@@ -0,0 +1,13 @@
+package com.k8.eks.EKS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8/eks/EKS/EksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8.eks.EKS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
